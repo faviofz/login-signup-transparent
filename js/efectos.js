ddaaedb47ecdd6ef10d1aff7d40eecb3dd250d81
Fz <favioo.wow@gmail.com>
2020-08-28 08:30:11 -0300

    Modificando efectos.js

--- js/efectos.js
+++ js/efectos.js
@@ -1,16 +1,15 @@
-function check_submit() {
-    const text = document.querySelector('input[type="text"]');
-    const password = document.querySelector('input[type="password"]');
-    const submit = document.querySelector('input[type="submit"]');
-    
-    if(text.value != '' && password.value != ''){
-        submit.removeAttribute('disabled');
-        submit.setAttribute('style', 'opacity: 1');
-    }
-    else {
-        submit.setAttribute('disabled', true);
-        submit.setAttribute('style', 'opacity: 0.5');
-    }
-}
+const form = document.querySelector("form");
 
+form.addEventListener("input", function () {
+	const email = document.querySelector('input[type="email"]');
+	const password = document.querySelector('input[type="password"]');
+	const submit = document.querySelector('input[type="submit"]');
 
+	if (email.value != "" && password.value != "") {
+		submit.removeAttribute("disabled");
+		submit.setAttribute("style", "opacity: 1");
+	} else {
+		submit.setAttribute("disabled", true);
+		submit.setAttribute("style", "opacity: 0.5");
+	}
+});
